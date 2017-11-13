ae8a383bd419643bd487950507a686a3d23cdf1c
Alex Killian <alex.killian@colorado.edu>
2017-11-13 11:47:26 -0700

    moved connection details to constants in the DatabaseUtil

--- src/main/java/dictionary/utils/DatabaseUtil.java
+++ src/main/java/dictionary/utils/DatabaseUtil.java
@@ -14,10 +14,25 @@ import org.hibernate.cfg.Configuration;
  */
 public class DatabaseUtil {
 
-	/*
+	/**
 	 * The name of the dictionary database.
 	 */
 	public static final String DB_NAME = "dictionarydb";
+	
+	/**
+	 * The host of the DB connection.
+	 */
+	public static final String HOST = "localhost";
+	
+	/**
+	 * The port of the DB connection.
+	 */
+	public static final int PORT = 3306;
+	
+	/**
+	 * If the DB is auto created or not.
+	 */
+	public static final boolean AUTO_CREATE_DB = true;
 	
 	/**
 	 * The name of the hibernate config file.
@@ -25,9 +40,28 @@ public class DatabaseUtil {
 	public static final String HIBERNATE_CFG_NAME = "hibernate.cfg.xml";
 	
 	/**
+	 * The Dev username for the DB.
+	 */
+	public static final String USERNAME = "root";
+	
+	/**
+	 * The Dev password for the DB.
+	 */
+	public static final String PASSWORD = "root";
+	
+	/**
 	 * The session factory for the while application.
 	 */
 	private static SessionFactory sessionFactory = null;
+	
+	/**
+	 * Constructs and returns the JDBC connection string.
+	 * 
+	 * @return String
+	 */
+	public static String getConnectionString() {
+		return "jdbc:mysql://" + HOST + ":" + PORT + "/" + DB_NAME + "?createDatabaseIfNotExist=" + AUTO_CREATE_DB;
+	}
 
 	/**
 	 * Returns the application's session factory.
@@ -38,6 +72,9 @@ public class DatabaseUtil {
 		if (sessionFactory == null) {
 			try {
 				Configuration config = new Configuration();
+				config.setProperty("hibernate.connection.url", getConnectionString());
+				config.setProperty("hibernate.connection.username", USERNAME);
+				config.setProperty("hibernate.connection.password", PASSWORD);
 				config.configure(HIBERNATE_CFG_NAME);
 				StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
 				sessionFactory = config.buildSessionFactory(serviceRegistry);
