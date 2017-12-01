ca8bda5c6f5dce6f2914da0367d5efe70d07d491
Adam <ajwieme@gmail.com>
2017-12-01 13:00:17 -0700

    adding commons-csv, and porterStemmer

--- src/main/java/dictionary/model/PorterStemmer.java
+++ src/main/java/dictionary/model/PorterStemmer.java
@@ -1,5 +1,20 @@
 package dictionary.model;
 
+/**
+ * 
+ * @author Adam Wiemerslage
+ *
+ * The porter stemmer implementation for stemming
+ */
 public class PorterStemmer {
-
+	int version;
+	
+	/**
+	 * This implements the porter stemmer algorithm for finding a stem given a word
+	 * @param input string
+	 * @return stemmed version of the string
+	 */
+	public String stem(String s) {
+		return s;
+	}
 }
