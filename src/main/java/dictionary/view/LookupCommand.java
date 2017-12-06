742aa48f3e42f1eea85af57b812c25b6a49e3872
Hunter Wapman <hunter.wapman@gmail.com>
2017-12-06 13:27:05 -0700

    fixed switch in removecommand; implemented lookupcommand

--- src/main/java/dictionary/view/LookupCommand.java
+++ src/main/java/dictionary/view/LookupCommand.java
@@ -1,7 +1,6 @@
 package dictionary.view;
 
 import dictionary.model.DictionaryEntry;
-import dictionary.model.WordForm;
 import dictionary.controller.Dictionary;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -18,24 +17,34 @@ public class LookupCommand extends DictionaryCommand {
 	
 	public LookupCommand(String query, String queryType) {
 		this.query = query;
-		this.queryType = "WordForm";
+		this.queryType = queryType;
 	}
 
 	@Override
 	void run() {
-		if (this.queryType.equals("WordForm")) {
-			entries = Dictionary.getInstance().lookupByEntry(this.query);
-
-			if (entries.size() != 0){
-				for (DictionaryEntry entry : entries){
-					System.out.println(entry.toString());					
-				}
-			} else {
-				System.out.print("No entries found.");
+		switch (queryType) {
+			case DICTIONARY_ENTRY_QUERY_TYPE: 
+				entries = Dictionary.getInstance().lookupByEntry(query);
+				break;
+			case DEFINITION_QUERY_TYPE: 
+				entries = Dictionary.getInstance().lookupByDefinition(query);
+				break;
+			case PART_OF_SPEECH_QUERY_TYPE: 
+				entries = Dictionary.getInstance().lookupByPartOfSpeech(query);
+				break;
+			default: 
+				break;
+		}
+			
+		if (entries.size() != 0){
+			for (DictionaryEntry entry : entries){
+				System.out.println(entry.toString());					
 			}
+		} else {
+			System.out.print("No entries found.");
 		}
 	}
-
+		
 	@Override
 	void displayStatus() {
 		// TODO Auto-generated method stub
--- src/main/java/dictionary/view/RemoveCommand.java
+++ src/main/java/dictionary/view/RemoveCommand.java
@@ -1,7 +1,6 @@
 package dictionary.view;
 
 import dictionary.controller.Dictionary;
-import dictionary.model.DictionaryEntry;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -15,7 +14,6 @@ public class RemoveCommand extends DictionaryCommand {
 	private String queryType;
 	private int idx;
 	private String wordForm;
-	private Collection<DictionaryEntry> entries = new ArrayList<DictionaryEntry>();
 	
 	public RemoveCommand(String query, String queryType) {
 		this.query = query;
@@ -35,13 +33,23 @@ public class RemoveCommand extends DictionaryCommand {
 	@Override
 	void run() {		
 		switch (queryType) {
-			case DICTIONARY_ENTRY_QUERY_TYPE: Dictionary.getInstance().removeDictionaryEntry(query);
-			case WORD_SENSE_QUERY_TYPE: Dictionary.getInstance().removeWordSense(query, idx);
-			case DEFINITION_QUERY_TYPE: Dictionary.getInstance().removeDefinition(query, idx);
-			case PART_OF_SPEECH_QUERY_TYPE: Dictionary.getInstance().removePartOfSpeech(query, idx);
-			case WORD_FORM_QUERY_TYPE: Dictionary.getInstance().removeWordForm(query, idx, wordForm);			
+			case DICTIONARY_ENTRY_QUERY_TYPE: 
+				Dictionary.getInstance().removeDictionaryEntry(query);
 				break;
-			default: break;
+			case WORD_SENSE_QUERY_TYPE: 
+				Dictionary.getInstance().removeWordSense(query, idx);
+				break;
+			case DEFINITION_QUERY_TYPE: 
+				Dictionary.getInstance().removeDefinition(query, idx);
+				break;
+			case PART_OF_SPEECH_QUERY_TYPE: 
+				Dictionary.getInstance().removePartOfSpeech(query, idx);
+				break;
+			case WORD_FORM_QUERY_TYPE:
+				Dictionary.getInstance().removeWordForm(query, idx, wordForm);
+				break;
+			default: 
+				break;
 		}
 	}
 
