9846d0cbff612cafdf10c8407d25e60ef5a8d20c
Hunter Wapman <hunter.wapman@gmail.com>
2017-12-06 14:13:39 -0700

    added update commands

--- src/main/java/dictionary/view/DictionaryCommand.java
+++ src/main/java/dictionary/view/DictionaryCommand.java
@@ -14,6 +14,7 @@ public abstract class DictionaryCommand extends AbstractCommand {
 	protected PartOfSpeech partOfSpeech;
 	protected WordForm wordForm;
 	protected WordSense wordSense;
+	protected String status;
 	
 	public static final String FILE_QUERY_TYPE = "file";
 	public static final String DICTIONARY_ENTRY_QUERY_TYPE = "entry";
@@ -21,4 +22,21 @@ public abstract class DictionaryCommand extends AbstractCommand {
 	public static final String PART_OF_SPEECH_QUERY_TYPE = "partofspeech";
 	public static final String DEFINITION_QUERY_TYPE = "definition";
 	public static final String WORD_FORM_QUERY_TYPE = "wordform";
+	
+	protected void setNotSupportedStatus(String queryType) {
+		this.status = queryType + " is not supported";
+	}
+	
+	protected String getIdentifierString(String query) {
+		return "Entry " + query;
+	}
+
+	protected String getIdentifierString(String query, int idx) {
+		return getIdentifierString(query) + ", Word Sense " + idx;
+	}
+
+	protected String getIdentifierString(String query, int idx, String wordForm) {
+		return getIdentifierString(query, idx) + ", Word Form " + wordForm;
+	}
+
 }
--- src/main/java/dictionary/view/UpdateCommand.java
+++ src/main/java/dictionary/view/UpdateCommand.java
@@ -8,12 +8,60 @@ import dictionary.controller.Dictionary;
  *
  */
 public class UpdateCommand extends DictionaryCommand {
+	private String queryType;
+	private String query;
 	private String target;
-
+	private String oldWordForm;
+	private String status;
+	private int idx;
+	
+	public UpdateCommand(String query, String queryType) {
+		this.query = query;
+		this.queryType = queryType;
+		this.status = "Haven't done anything yet.";
+	}
+	
+	public UpdateCommand(String query, String queryType, String target) {
+		this(query, queryType);
+		this.target = target;
+	}
+	
+	public UpdateCommand(String query, String queryType, int idx, String target) {
+		this(query, queryType);
+		this.idx = idx;
+		this.target = target;
+	}
+	
+	public UpdateCommand(String query, String queryType, int idx, String oldWordForm, String target) {
+		this(query, queryType, idx, target);
+		this.oldWordForm = oldWordForm;
+	}
+	
 	@Override
 	void run() {
-		// TODO Auto-generated method stub
-
+		switch (queryType) {
+			case DICTIONARY_ENTRY_QUERY_TYPE: 
+				Dictionary.getInstance().updateDictionaryEntry(query, target);
+				status = getIdentifierString(query) + ": set Word Root to " + target;
+				break;
+			case DEFINITION_QUERY_TYPE: 
+				Dictionary.getInstance().updateDefinition(query, idx, target);
+				status = getIdentifierString(query, idx) + ": set Definition to " + target;
+				break;
+			case WORD_SENSE_QUERY_TYPE:
+				setNotSupportedStatus(WORD_SENSE_QUERY_TYPE);
+				break;
+			case PART_OF_SPEECH_QUERY_TYPE: 
+				Dictionary.getInstance().updatePartOfSpeech(query, idx, target);
+				status = getIdentifierString(query, idx) + ": set Part of Speech to " + target;
+				break;
+			case WORD_FORM_QUERY_TYPE:
+				Dictionary.getInstance().updateWordForm(query, idx, oldWordForm, target);
+				status = getIdentifierString(query, idx, oldWordForm) + ": set Word Form to " + target;
+				break;
+			default: 
+				break;
+		}
 	}
 
 	@Override
